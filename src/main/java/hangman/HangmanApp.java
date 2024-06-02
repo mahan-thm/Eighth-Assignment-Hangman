f6cdd29704a1969881b259dc40c3ea98bb0d2f48
moon <m.tahmasbi1383@gmail.com>
2024-06-03 02:09:48 +0330

    update: hangman.fxml

--- src/main/java/hangman/HangmanApp.java
+++ src/main/java/hangman/HangmanApp.java
@@ -10,8 +10,12 @@ import java.io.IOException;
 public class HangmanApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("hangman-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("entry/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("game/hangman.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
         stage.setTitle("Hangman");
         stage.setScene(scene);
         stage.show();
